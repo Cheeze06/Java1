726c014c0f01e7e1b07eec5639efdac4ae2118f0
Cheeze06 <74716470+Cheeze06@users.noreply.github.com>
2024-01-15 10:49:09 +0900

    Update Ch09.java

--- Ch09.java
+++ Ch09.java
@@ -3,17 +3,100 @@ import java.util.Scanner;
 public class Ch09 {
 
 	public static void main(String[] args) {
-		//1부터 n(입력한 수)까지의 합 구하기
-		Scanner sc = new Scanner(System.in);
-//		System.out.println("몇까지 더할까요?");
-//		int n = sc.nextInt();
-//		int i = 1;
-//		int sum= 0;
-//		while(i <= n) {
-//			sum += i;
-//			i++;
+
+		
+		// 00 반복문
+		// 반복적인 실행이 필요할 때 반복문을 사용함.
+		
+		// 01 while문의 구조
+		// 조건식이 참인 경우 반복적으로 코드를 실행한다.
+		
+		
+//		while (조건) {
+//			반복 실행 코드;			// 예) 조건이 참이면 코드 실행
+//								//     조건이 거짓이면 반복문 종료
 //		}
-//		System.out.printf("1부터 %d까지의 합은 %d입니다.", n, sum);
+		
+		
+//		System.out.println("--------------- while문 예제 01 ---------------");
+////      	// 탈출용 변수
+//     		int i = 1;
+//
+// 		// 탈출용 조건식
+//      	while (i <= 10) {
+//          		System.out.println("Hello World");
+//          		i++;  // 탈출을 위한 연산식
+//          
+//      	}
+      
+      
+      
+      		System.out.println("--------------- while문 예제 02 ---------------");
+//    		1부터 10까지의 합 구하기
+//    		int i = 1;
+//    		int sum = 0;
+//
+//  		  while (i <= 10) {
+//        		System.out.println("i = " + i);
+//        		sum += i; 							// sum = sum + i;
+//   			i++;      							// 후치연산
+//    		}	
+//
+//  		System.out.println("while 종료 후 i의 값 = " + i);
+//   		System.out.println("1부터 10까지의 합 = " + sum);
+      
+      
+      
+//    		1부터 N(입력한 수)까지의 합 구하기
+//      	System.out.println("정수 N을 하나 입력하세요, 제가 1부터 N까지의 수의 합을 구해드릴게요!");
+//      	Scanner sc = new Scanner(System.in);
+//      	int n = sc.nextInt();
+//      
+//      	int i = 1;
+//      	int sum = 0;
+//      
+//      	while(i <= n) {
+//    			sum += i;				// N까지 반복하면서 i를 
+//    			i++;					// 증가하면서 sum이라는 변수 공간에 저장
+//      	}
+//      
+//      	System.out.println("[SYSTEM] : 1부터 " + n + "까지의 수의 합은 " + sum + "입니다.");
+      
+      
+      
+      
+      
+    		//문제
+     		System.out.println("--------------- while문 문제 01 ---------------");
+      		//정수 두개를 입력 받아(n,m) n부터 m까지의 합을 구하세요
+      		//이때 받는 수는 n이 m보다 작아야 합니다(제약조건)
+      
+      
+      
+//		System.out.println("정수1을 입력하세요 >>> ");
+//		int num1 = scanner.nextInt();
+//		int num11 = num1;
+//	
+//		System.out.println("정수2를 입력하세요 >>> ");
+//		int num2 = scanner.nextInt();
+//		int num21 = num2;
+//	
+//		int sum1 = 0;
+//	
+//		if (num2 > num1) {
+//			while (num11 <= num2) {
+//				sum1 += num11;
+//				num11++;
+//			}
+//			System.out.printf("%d부터 %d까지의 합은 %d입니다.", num1, num2, sum1);
+//		}
+//		else if (num1 >= num2) {
+//			while (num21 <= num1 ) {
+//				sum1 += num21;
+//				num21++;
+//			}
+//		System.out.printf("%d부터 %d까지의 합은 %d입니다.", num2, num1, sum1);
+//		}
 //		
 		
 		
@@ -68,7 +151,27 @@ public class Ch09 {
 //			}
 //			x--;
 //		}
-		
+
+		//03  while - 무한 루프 이용하기
+      		System.out.println("--------------- while - 무한 루프 예제 ---------------");
+   		// 값을 사용자로부터 입력받아 모두 더하는 프로그램을 만들어보자 !!
+   		// 값이 -1이라면 프로그램 종료 !!
+      
+      		Scanner sc = new Scanner(System.in);
+      		int num;
+      		int sum = 0;
+      
+      		while(true) {
+    	  		System.out.println("정수를 하나 입력해요!! 단, -1을 입력한다면 종료시킬껍니다!!");
+    	  		num = sc.nextInt();
+    	  
+    	  		if(num == -1) {
+	    		  	break;
+    			}
+    	 		sum += num;   	  
+      		}
+      
+      		System.out.println("여러분이 입력한 수들의 합은 " + sum);
 		
 		// continue 예약어
 		int i = 1;
